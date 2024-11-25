05576dde5b3207f5b2dd11630062e40cd0d0afac
cdntinker <58090022+cdntinker@users.noreply.github.com>
2024-11-24 23:00:14 -0500

    Update DEBUG_functions.cpp

--- src/DEBUG_functions.cpp
+++ src/DEBUG_functions.cpp
@@ -207,8 +207,6 @@ void DEBUG_ESP_info()
     DEBUG_LineOut(Line);
     sprintf(Line, " ESP SDK version: %s", ESP.getSdkVersion());
     DEBUG_LineOut(Line);
-    sprintf(Line, "        Ram Size: %d", ESP.getHeapSize());
-    DEBUG_LineOut(Line);
     sprintf(Line, "       Free heap: %d", ESP.getFreeHeap());
     DEBUG_LineOut(Line);
 }
