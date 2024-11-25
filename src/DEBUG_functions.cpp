5991ce5d9f254d0b84e32c98c5de4b17b25879e9
cdntinker <cdntinker@gmail.com>
2024-11-25 17:01:06 -0500

    ...

--- src/DEBUG_functions.cpp
+++ src/DEBUG_functions.cpp
@@ -220,6 +220,7 @@ uint32_t ChipID = 0;        // ESP efuse ID
     }
 uint32_t ChipCores = ESP.getChipCores()
 uint32_t FlashID = ESP.getFlashChipId();
+uint32_t FlashSize = ESP.getFlashChipSize();
 #endif
 
     DEBUG_SectionTitle("ESP Info");
