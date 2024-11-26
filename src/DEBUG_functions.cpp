031f68f181764c9431f350dbaa356c8ad807f306
cdntinker <cdntinker@gmail.com>
2024-11-25 21:13:29 -0500

    .

--- src/DEBUG_functions.cpp
+++ src/DEBUG_functions.cpp
@@ -234,7 +234,7 @@ void DEBUG_ESP_info()
     sprintf(Line, "     Flash speed: %u", FlashSpeed);
     DEBUG_LineOut(Line);
 #if defined(ESP32)
-double PSinM = PSramSize / 1024 / 1024;
+double PSinM = PSramSize / 1024,0 / 1024.0;
     sprintf(Line, "      Psram Size: %d (%f MB)", PSramSize, PSinM);
     // sprintf(Line, "      Psram Size: %d (%d MB)", PSramSize, PSramSize / 1024 / 1024);
     DEBUG_LineOut(Line);
