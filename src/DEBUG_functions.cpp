16bc2de69099a2870ad5b7294de318df8bcc60ed
cdntinker <cdntinker@gmail.com>
2024-11-30 19:36:56 -0500

    Tryin' to make DEBUG_WiFi_info() deal fully with AP, STA & AP+STA modes...

--- src/DEBUG_functions.cpp
+++ src/DEBUG_functions.cpp
@@ -460,8 +460,8 @@ void DEBUG_ESP_info()
 ////////////////////////////////////////////////////////////////
 //                   WiFi Related Debugging                   //
 ////////////////////////////////////////////////////////////////
-extern const char* APssid;
-extern const char* APpass;
+extern char* APssid;
+extern char* APpass;
 
 void DEBUG_WiFi_info()
 {
